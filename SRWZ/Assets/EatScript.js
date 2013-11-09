bbe4ba5680f35b190a4d3cbeb8f718c51b81525a
ChrisGermano <germano.c@husky.neu.edu>
2013-11-08 22:51:16 -0500

    Added (very) initial sidescrolling prototype

---
+++ SRWZ/Assets/EatScript.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+private var canEat : boolean;
+public var eatCD : float;
+private var eatCount : float;
+
+
+
+function Start () {
+	eatCount = 0;
+	canEat = true;
+}
+
+function LateUpdate () {
+	
+	if (eatCount < eatCD) {
+		eatCount++;
+	}
+	
+	var eatPercentage : float = (eatCount/eatCD);
+	var eatBar : Color = new Color(0f, eatPercentage, 0f);
+	GameObject.FindGameObjectWithTag("PlayerBite").renderer.material.color = eatBar;
+	
+	if (eatCount == eatCD) {
+		canEat = true;
+		Debug.Log("Can Eat");
+	}
+}
+
+function OnTriggerStay(col : Collider) {
+	if (Input.GetKeyDown("space")) {
+		if (canEat && col.gameObject.tag == "Zombie") {
+			col.gameObject.renderer.enabled = false;
+		}
+		Debug.Log("Can't Eat");
+		canEat = false;
+		eatCount = 0;
+	}
+}
---
+++ SRWZ/Assets/SpawnScript.js
@@ -0,0 +1,70 @@
+﻿#pragma strict
+
+public var zombie : GameObject;
+
+public var xPlaces : int[];
+public var spawnedZombies : GameObject[];
+private var xCount : int;
+
+public var spawnRate : int;
+private var spawnTick : int;
+
+public var moveTime : float;
+public var moveDist : int;
+
+function Start () {
+	spawnTick = 0;
+	xCount = 0;
+	spawnedZombies = new GameObject[xPlaces.Length];
+}
+
+function Update () {
+	spawnTick++;
+	if (spawnTick >= spawnRate) {
+		spawnTick = 0;
+		xCount++;
+		var doneCheck : boolean = true;
+		var fullZoms : boolean = false;
+		for (var i : int = 0; i < xPlaces.Length; i++) {
+			if (xCount == xPlaces[i]) {
+				
+				if (xPlaces[i] != -1) {
+					doneCheck = false;
+				}
+				var newZom : GameObject = GameObject.Instantiate(zombie, new Vector3(30,2,0), Quaternion.identity);
+				spawnedZombies[i] = newZom;
+				if (i == xPlaces.Length-1) {
+					fullZoms = true;
+				}
+				move(newZom);
+				xPlaces[i] = -1;
+			}
+		}
+		if (doneCheck && fullZoms) {
+			var ateCount : int = 0;
+			for (var x : int = 0; x < xPlaces.Length; x++) {
+				if (!spawnedZombies[x].renderer.enabled) {
+					ateCount++;
+				}
+			}
+		}
+	}
+}
+
+function move(zombie : GameObject) {
+	yield StartCoroutine("moveZom",zombie);
+}
+
+function moveZom(zom : GameObject) {
+	var timeSpent : float = 0;
+	var startPos : Vector3 = zom.transform.position;
+	var newPos : Vector3 = startPos;
+	newPos.x -= moveDist;
+	
+	
+	while (timeSpent < moveTime) {
+		zom.transform.position = Vector3.Lerp(startPos, newPos, (timeSpent/moveTime));
+		timeSpent += Time.deltaTime;
+		yield;
+	}
+}
