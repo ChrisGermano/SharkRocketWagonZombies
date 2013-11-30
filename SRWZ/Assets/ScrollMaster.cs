00b44e3445429f21dce9d3efc0209466d7bc573f
Eric Peterson <ericpeterson@Erics-MacBook-Pro.local>
2013-11-30 01:55:17 -0500

    Some visual polish
    Added scrolling to stage 2, different texture for stage 1

---
+++ SRWZ/Assets/ScrollMaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollMaster : MonoBehaviour {
+
+	public float scroll_speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		//Scroll the grass on the ground
+		float offset = Time.time * scroll_speed * -2;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+	}
+}
